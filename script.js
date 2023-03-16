f569ea12e1b0abb403b280e175b851c88505ca7a
Michel Geoffroy <geoffroy827@gmail.com>
2023-03-16 15:28:08 +0100

    modified file script

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const navBar = document.querySelector('#navSite');
-const about = document.querySelector('#about');
 
-
-const scrolltopButton = document.querySelector('#scroll-top-button');
+const navbar = document.getElementById("navSite");
+const scrollTopButton = document.getElementById("scroll-top-button");
 
 window.onscroll=function(){
-  if(window.pageYOffset>200){
-    scrolltopButton.classList.add("show");
-  }else{
-    scrolltopButton.classList.remove("show");
-  }
+    if(window.pageYOffset>200){
+        navbar.classList.remove("bg-transparent", "navbar-dark");
+        navbar.classList.add("bg-light", "navbar-light");
+        scrollTopButton.classList.add("show");
+    }
+    else{
+        navbar.classList.add("bg-transparent", "navbar-dark");
+        navbar.classList.remove("bg-light", "navbar-light");
+        scrollTopButton.classList.remove("show");
+    }
 }
 
 
